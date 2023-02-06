e6a2f7bd3fe554763691156eba9d337a91295c80
Casey Sytsema <sytsemacj@gmail.com>
2023-02-05 21:40:43 -0500

    Messages reach destination

--- project1.c
+++ project1.c
@@ -10,14 +10,12 @@ void endGracefully(int n);
 void nodeProcess(int pid);
 
 struct messageHeader{
-    int src;
     int dest;
     char message[100];
 };
 
 int k;
 int pipes[MAX_LEN][2];
-int delivered = 0;
 
 
 int main(void){
@@ -41,6 +39,7 @@ int main(void){
             nodeProcess(i);
             exit(0);
         }
+        nodes[i] = pid;
     }
     
     char message[100];
@@ -48,58 +47,53 @@ int main(void){
     struct messageHeader hdr;
 
     while(1){
-        if (!delivered){
-            // get mesage and destination
-            printf("Enter a message to send ~ ");
-            scanf("%s", message);
+        // get mesage and destination
+        printf("Enter a message to send ~ ");
+        scanf("%s", message);
 
-            while(1){
-                printf("Enter a node to send it to (between 0 and %d) ~ ", k - 1);
-                scanf("%d", &dest);
+        while(1){
+            printf("Enter a node to send it to (between 0 and %d) ~ ", k - 1);
+            scanf("%d", &dest);
 
-                if (dest < 0 || dest >= k){
-                    printf("Invalid node. Try again");
-                }
-                else{
-                    break;
-                }
+            if (dest < 0 || dest >= k){
+                printf("Invalid node. Try again");
+            }
+            else{
+                break;
             }
         }
-
+        
         // put data into struct
-        hdr.src = 0;
         hdr.dest = dest;
         strcpy(hdr.message, message);
 
         // install sig handler
         signal(SIGINT, endGracefully);
         sleep(1);
-        write(pipes[dest][1], &hdr, sizeof(hdr));
-        delivered = 0;
+        write(pipes[0][1], &hdr, sizeof(hdr));
     }
 
     return 0;
 }
 
 void nodeProcess(int id){
+
     int left = (id + k - 1) % k;
-    int right = (id + k) % k;
+    int right = (id + k) % k; // neat trick courtesy of chatGPT
 
     while(1){
         struct messageHeader hdr;
-        read(pipes[left][0], &hdr, sizeof(hdr)); // read the header into local struct
-        if (hdr.dest == id){ // if this node is the destination for the message
-            printf("Node %d received a message from Node %d -- %s\n", id, hdr.src, hdr.message);
-            hdr.dest = -1;
-            delivered = 1;
-            strcpy(hdr.message, "");
-        }
-        else{ // if the message is not meant for this node
-            printf("Node %d received the message, but it was not meant for it. Forwarding to node %d\n", id, right);
-            hdr.src = id;
-            sleep(1);
-            write(pipes[right][1], &hdr, sizeof(hdr));
-            break;
+        if (read(pipes[left][0], &hdr, sizeof(hdr)) > 0){
+            if (hdr.dest == id){ // if this node is the destination for the message
+                printf("Node %d received a message from Node %d -- %s\n", id, left, hdr.message);
+                break;
+            }
+            else{ // if the message is not meant for this node
+                printf("Node %d received the message from node %d, but it wasn't meant for it. Forwarding to node %d\n", id, left, right + 1);
+                sleep(1);
+                write(pipes[id][1], &hdr, sizeof(hdr));
+                break;
+            }
         }
     }
 }
